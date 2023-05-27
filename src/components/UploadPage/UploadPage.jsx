58284b9ae0073ac5c78cdd5c87c27a820e368d6e
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-05-27 13:11:58 -0700

    styled upload page and added form submission

--- src/components/UploadPage/UploadPage.jsx
+++ src/components/UploadPage/UploadPage.jsx
@@ -1,34 +1,47 @@
 import './UploadPage.scss';
 import publishIcon from '../../assets/icons/publish.svg';
-import videoPreview from '../../assets/images/Upload-video-preview.jpg'
+import videoPreview from '../../assets/images/Upload-video-preview.jpg';
 
 function UploadPage() {
-  return (
-    <main id='ul-page'>
+  const submitForm = () => {
+    alert('Form submitted successfully!');
+    window.location.href = '/';
+  };
 
-      <section>
+  const clickHandler = () => {
+    submitForm();
+  };
+
+  return (
+    <main id="upload-page">
+      <section className="up-head">
         <h1>Upload Video</h1>
       </section>
 
-      <div className='ulp'>
-      <div>
-        <span>Video Thumbnail</span>
-          <img src={videoPreview} alt="" className="video-preview" />
-      </div>
-        <form className='up-form'>
-          <label htmlFor="video-title">Title your video:</label>
-          <textarea id="video-title" name="video-title" placeholder="Add a title to your video"></textarea>
-          <label htmlFor="video-description">Video description:</label>
-          <textarea id="video-description" name="video-description" placeholder="Add a description to your video"></textarea>
+      <div className="up-wrap">
+        <div className="up-wrap__video">
+          <span>Video Thumbnail</span>
+          <img src={videoPreview} alt="video-title" />
+        </div>
 
-          <button className="btn" >
-          <img src={publishIcon} alt="publish" />
-          <span>Publish</span>
-        </button>
+        <form className="up-wrap__form" onSubmit={submitForm}>
+          <label className="up-wrap__form--titlelabel" htmlFor="video-title">
+            Title your video </label>
+          <textarea className="up-wrap__form--titletext" id="video-title" name="video-title" placeholder="Add a title to your video"></textarea>
 
-        <button type="button">Cancel</button>
-
+          <label className='up-wrap__form--desclabel' htmlFor="video-description">
+            Add a video description </label> <textarea className="up-wrap__form--desctext" id="video-description" name="video-description" placeholder="Add a description to your video"></textarea>
         </form>
+
+        <div className="up-sub">
+          <button className="btn up-sub__publish" type="submit" onClick={clickHandler}>
+            <img src={publishIcon} alt="publish" />
+            <span>Publish</span>
+          </button>
+          <button className="btn up-sub__cancel" type='button'>
+            <span>Cancel</span>
+          </button>
+        </div>
       </div>
     </main>
   );
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,41 +1,24 @@
 import './VideoList.scss';
 import { withEllipsis } from '../utils/utils';
-import axios from 'axios';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function VideoList({ videoList, setVideoList, activeVideo, handleChangeActiveVideo  }) {
-  console.log('videoList:', videoList);
-
-
-  
-
-
-
-  
+function VideoList({ videoList }) {
   return (
     <>
       {videoList
         .filter((activeVideo) => activeVideo.id !== videoList.id)
         .map((activeVideo) => {
-
           const videoTitleToEllipsis = withEllipsis(activeVideo.title);
-
           const divStyle = {
-            backgroundImage: `url(${activeVideo.image})`, // used chatGPT for this line of code
+            backgroundImage: `url(${activeVideo.image})`,
           };
-
           return (
-
-            
             <Link
               key={activeVideo.id}
               className="video-list-item"
               to={`/videos/${activeVideo.id}`}
-              // onClick={() => handleChangeActiveVideo(activeVideo.id)}
             >
-              {/* {video.id} */}
               <div className='video-list-item__image' style={divStyle}></div>
               <div className='video-list-item__wrapper'>
                 <div className='video-list-item__wrapper--title'>{videoTitleToEllipsis}</div>
