340943342452bce6748b2da7e41ab037c79e6633
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-05-28 13:17:00 -0700

    tweaked css borders.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UploadPage from './components/UploadPage/UploadPage';
 import Header from './components/Header/Header';
 import VideoPage from './components/VideoPage/VideoPage';
-import VideoList from './components/VideoList/VideoList';
 
 function App() {
 
--- src/components/VideoPage/VideoPage.jsx
+++ src/components/VideoPage/VideoPage.jsx
@@ -9,38 +9,38 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function VideoPage() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [video] = useState([]);
   const [videoList, setVideoList] = useState([]);
   const [activeVideo, setActiveVideo] = useState({});
 
   useEffect(() => {
     axios
-    .get(`https://project-2-api.herokuapp.com/videos/?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
-    .then((response) => {
-      setVideoList(response.data);
+      .get(`https://project-2-api.herokuapp.com/videos/?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
+      .then((response) => {
+        setVideoList(response.data);
 
-      if (!id) {
-        axios 
-        .get(`https://project-2-api.herokuapp.com/videos/${response.data[0].id}?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
-        .then((response) => {
-          setActiveVideo(response.data);
-        })
-      }
-    });
+        if (!id) {
+          axios
+            .get(`https://project-2-api.herokuapp.com/videos/${response.data[0].id}?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
+            .then((response) => {
+              setActiveVideo(response.data);
+            })
+        }
+      });
   }, []);
 
   useEffect(() => {
 
-    if(id) {
-      axios 
-      .get(`https://project-2-api.herokuapp.com/videos/${id}?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
-      .then((response) => {
-        setActiveVideo(response.data);
-      })
+    if (id) {
+      axios
+        .get(`https://project-2-api.herokuapp.com/videos/${id}?api_key=92ff28cd-7e73-4069-852a-6a0708f75739`)
+        .then((response) => {
+          setActiveVideo(response.data);
+        })
     }
 
-  },[id]);
+  }, [id]);
 
   const handleChangeActiveVideo = (id) => {
     const foundVideo = video.find((video) => video.id === id);
@@ -49,8 +49,8 @@ function VideoPage() {
 
   return (
     <main>
-      
-       <VideoPlayer activeVideo={activeVideo} />
+
+      <VideoPlayer activeVideo={activeVideo} />
       <div className='pagecontainer'>
         <div className='pagecontainer__left'>
           <VideoInfo videoList={videoList} activeVideo={activeVideo} formatDate={formatDate} />
