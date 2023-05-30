2f405e8098b9575d9ff012b078a53ba98289b38f
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-05-30 16:01:54 -0700

    connected api properly.

--- index-Philips-Surface.js
+++ index-Philips-Surface.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const app = express();
 const videosRoutes = require('./routes/videos');
-const cors = require('cors');
+
 
 require('dotenv').config();
 const PORT = process.env.PORT;
 
 app.use(express());
 
-app.use(cors({
-  origin: 'http://localhost:3000' // Replace with your desired origin
-}));
-
-app.use(express.static('public'));
-
 app.use('/videos', videosRoutes);
 
 // home route
 app.get('/', (req, res) => {
-  console.log('SHE WORK');
-  res.send('HE WORK');
+    console.log('SHE WORK');
+    res.send('HE WORK');
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+    console.log(`App running on port ${PORT}`);
 });
--- routes/videos.js
+++ routes/videos.js
@@ -2,19 +2,60 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const publicPath = 'http://localhost:' + PORT + '/images/';
+
+
+router.get('/:id', (req, res) => {
+
+const videoId = req.params.id;
+
+  fs.readFile('./data/videos.json', 'utf-8', (err, data) => {
+      if (err) {
+        return res.send('an error ocurred reading videos data');
+      } else {
+        
+        const videos = JSON.parse(data);
+        let selectedVideo = videos.find(
+          (video) => video.id === videoId
+        );
+        selectedVideo = {...selectedVideo, image: publicPath + selectedVideo.image};
+        console.log('selectedVideo spread: ', selectedVideo)
+        // update image property using spread operator
+        
+
+
+        res.json(selectedVideo); 
+      }
+  });
+});
+
+
 router.get('/', (req, res) => {
 
-    console.log('beep beep');
-    // res.send('beeeeeeeeeep beeeeeeeeeep');
+  fs.readFile('./data/videos.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.send('an error ocurred reading videos data');
+    } else {
+      const videosParsed = JSON.parse(data);
 
-    fs.readFile('./data/videos.json', 'utf-8', (err, data) => {
-        if (err) {
-          return res.send('an error ocurred reading videos data');
-        } else {
-          res.json(JSON.parse(data)); 
-        }
-    });
+
+      const videoFiltered = videosParsed.map((video) => {
+        return ({
+          id: video.id,
+          title: video.title,
+          image: publicPath + video.image,
+          channel: video.channel,
+        })
+
+      })
+    
+      res.json(videoFiltered);
+    }
   });
+});
 
 
 
