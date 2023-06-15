83bdb4a953756d3089a16d81929769948a6711dc
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-06-14 21:23:21 -0700

    add swiper.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
-import HomePage from './Pages/HomePage/HomePage';
+import HomePage from './Pages/HomePage/HomePage'; 
 
 function App() {
   return (
--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,3 +1,4 @@
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -5,12 +6,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 export default () => {
+
+    
   return (
     <Swiper
       spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+    //   onSlideChange={() => console.log('slide change')}
+    //   onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
