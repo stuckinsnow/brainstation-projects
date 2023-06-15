feec77da6d5b577e239ee74bcec5a0682356f41f
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-15 10:23:26 -0700

    set up styling.

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,7 +1,9 @@
+import './ContactPage.scss';
+
 export default function ContactPage() {
     return (
         <>
-        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta tempore molestiae at iste vero eius aspernatur blanditiis architecto, possimus alias omnis delectus optio doloremque velit animi dolor quidem necessitatibus dolorum?</div>
+        <div className="contact-area">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta tempore molestiae at iste vero eius aspernatur blanditiis architecto, possimus alias omnis delectus optio doloremque velit animi dolor quidem necessitatibus dolorum?</div>
         </>
     );
 }
