2d21b93a5087a6a3938c14a0ba818ad4fb874a6e
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-06-14 21:42:46 -0700

    add carousel styling.

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,25 +1,30 @@
-
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
+import "swiper/css";
+import "swiper/css/navigation";
 
-export default () => {
+import "./Carousel.scss";
 
-    
+// import required modules
+import { Navigation } from "swiper";
+
+export default function App() {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-    //   onSlideChange={() => console.log('slide change')}
-    //   onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
-};
+}
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,22 +1,15 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import './HomePage.scss';
+import Carousel from '../../Components/Carousel/Carousel';
 
-// Import Swiper styles
-import 'swiper/css';
+function HomePage() {
+    return (
 
-export default () => {
-  return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-  );
-};
+      <>
+        <Carousel />
+        <div className=''>BEEP BEEP</div>
+      </>
+
+    );
+}
+
+export default HomePage;
