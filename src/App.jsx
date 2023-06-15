f96cf9710f7a084d0712c32ac40eea68bf32fe42
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-15 10:07:59 -0700

    add contact page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage/HomePage'; 
+import ContactPage from './Pages/ContactPage/ContactPage';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+
         </Routes>
       </BrowserRouter>
 
---
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+    return (
+        <>
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta tempore molestiae at iste vero eius aspernatur blanditiis architecto, possimus alias omnis delectus optio doloremque velit animi dolor quidem necessitatibus dolorum?</div>
+        </>
+    );
+}
