45efb3cff54e6104f538f844849772b921e29e85
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-06-17 22:40:27 -0700

    add exif library function.

--- migrations/create_photo_tables.js
+++ migrations/create_photo_tables.js
@@ -1,14 +1,16 @@
 exports.up = function (knex) {
-    return knex.schema
-      .createTable('photoalbum', (table) => {
-        table.increments('id').primary();
-        table.string('image').notNullable();
-        table.string('photo_name').notNullable().defaultTo('Photo');
-        table.string('photo_longitude').notNullable().defaultTo('N/A');
-        table.string('photo_latitude').notNullable().defaultTo('N/A');
-      });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable('photoalbum');
-  };
+  return knex.schema.createTable('photoalbum', (table) => {
+    table.increments('id').primary();
+    table.string('image');
+    table.string('filename').notNullable().defaultTo('');
+    table.string('photo_name').notNullable().defaultTo('Photo');
+    table.string('photo_longitude').notNullable().defaultTo('N/A');
+    table.string('photo_latitude').notNullable().defaultTo('N/A');
+    table.json('exif_data').nullable().defaultTo(null);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('photoalbum');
+};
--- server.js
+++ server.js
@@ -1,22 +1,60 @@
-const express = require("express");
+const express = require('express');
 require('dotenv').config();
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const PORT = process.env.PORT || 5050;
-const DB = process.env.DB_NAME;
+const multer = require('multer');
+const exifAsync = require('exif-async');
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
 
-const photoRoutes = require("./routes/photoRoutes");
-
-// console.log(process.env.DB_NAME);
+const upload = multer({ dest: 'uploads/' });
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (_req, res) => {
-    res.send("Welcome to my API");
+  res.send('Welcome to my API');
 });
 
+app.post('/upload', upload.single('photo'), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ error: 'No file provided' });
+    return;
+  }
 
-app.use("/photos", photoRoutes);
+  const imagePath = req.file.path;
 
-app.listen(PORT, () => console.log(`server running at http://localhost:${PORT}`));
+  exifAsync(imagePath)
+    .then((exifData) => {
+      exifData = exifData || {};
+
+      console.log('EXIF data:', exifData);
+
+      knex('photoalbum')
+        .insert({
+          filename: req.file.originalname,
+          exif_data: JSON.stringify(exifData),
+        })
+        .then(() => {
+          res.json({ message: 'Upload successful' });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to read EXIF data' });
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
