3e8143538cd66b3b5d5b7ba0ee0a97862233b5bd
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-26 16:15:16 -0700

    fix footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className='footer'>
-      <Link to="/">Portfolio</Link> 
+      <Link to="/">Portfolio</Link>
     </footer>
   )
 }
