345f825e39ffd7227e3d3d1f6fda072df1381e2c
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-26 16:14:03 -0700

    fix footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className='footer'>
-      <Link to="/">Portfolio</Link>
-      <Link to="/">LinkedIn URL can go here</Link>
-
-      {/* This can point to the sql database, if there's time to implement it. */}
-
+      <Link to="/">Portfolio</Link> 
     </footer>
   )
 }
