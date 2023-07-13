e237765be288681c6420e17f79733bc0bed16a23
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-07-13 15:10:20 -0700

    change routes.

--- src/Pages/PhotoPage/PhotoPage.jsx
+++ src/Pages/PhotoPage/PhotoPage.jsx
@@ -17,7 +17,10 @@ const PhotoPage = () => {
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/photos/`)
       .then(response => response.json())
-      .then(data => setPhotoData(data))
+      .then(data => {
+        setPhotoData(data);
+        console.log(data); 
+      })
       .catch(error => console.error(error));
   }, []);
   const handleDelete = () => {
@@ -38,49 +41,47 @@ const PhotoPage = () => {
   const pds = photoData?.exif_data?.gps;
   const googleA = 'https://www.google.com/maps/search/?api=1&query=';
 
-  console.log(photoData);
-
   return (
 
-    
     <>
-      {photoData ? (
-        <div className='delete-photo'>
+      {photoData ?
+        (
+          <div className='delete-photo'>
 
-          <img src={getImageUrl(photoData.filename)} alt={photoData.filename} />
-          <div className='delete-photo__writing'>
-            <h2 className='delete-photo__title'>Photo Information</h2>
-            <div className='delete-photo__about'>
-              <p>
-                <strong>Name:</strong> {photoData.photo_name}
-              </p>
-              <p>
-                <strong>Region:</strong> {photoData.photo_region}
-              </p>
-            </div>
+            <img src={getImageUrl(photoData.filename)} alt={photoData.filename} />
+            <div className='delete-photo__writing'>
+              <h2 className='delete-photo__title'>Photo Information</h2>
+              <div className='delete-photo__about'>
+                <p>
+                  <strong>Name:</strong> {photoData.photo_name}
+                </p>
+                <p>
+                  <strong>Region:</strong> {photoData.photo_region}
+                </p>
+              </div>
 
-            <h2 className='delete-photo__title delete-photo__title--margin'>EXIF Data</h2>
-            <div className="delete-photo__exif-data">
-              <p>
-                GPS: <Link className="gps" to={`${googleA}${formatGpsData(pds?.GPSLatitude, pds?.GPSLatitudeRef)},${formatGpsData(pds?.GPSLongitude, pds?.GPSLongitudeRef)}`} target="_blank" rel="noopener noreferrer">{formatGpsData(pds?.GPSLatitude, pds?.GPSLatitudeRef)} {formatGpsData(pds?.GPSLongitude, pds?.GPSLongitudeRef)}</Link>
-              </p>
-              <p>Date Taken: {photoData?.exif_data?.exif?.CreateDate}</p>
-              <p>Lens: {photoData?.exif_data?.exif?.LensModel}</p>
-              <p>Focal Length: {photoData?.exif_data?.exif?.FocalLength}mm</p>
-              <p>Shutter Speed: {formatExposureTime(photoData?.exif_data?.exif?.ExposureTime)}s</p>
-              <p>Aperture: f/{photoData?.exif_data?.exif?.FNumber}</p>
-              <p>ISO: {photoData?.exif_data?.exif?.ISO}</p>
+              <h2 className='delete-photo__title delete-photo__title--margin'>EXIF Data</h2>
+              <div className="delete-photo__exif-data">
+                <p>
+                  GPS: <Link className="gps" to={`${googleA}${formatGpsData(pds?.GPSLatitude, pds?.GPSLatitudeRef)},${formatGpsData(pds?.GPSLongitude, pds?.GPSLongitudeRef)}`} target="_blank" rel="noopener noreferrer">{formatGpsData(pds?.GPSLatitude, pds?.GPSLatitudeRef)} {formatGpsData(pds?.GPSLongitude, pds?.GPSLongitudeRef)}</Link>
+                </p>
+                <p>Date Taken: {photoData?.exif_data?.exif?.CreateDate}</p>
+                <p>Lens: {photoData?.exif_data?.exif?.LensModel}</p>
+                <p>Focal Length: {photoData?.exif_data?.exif?.FocalLength}mm</p>
+                <p>Shutter Speed: {formatExposureTime(photoData?.exif_data?.exif?.ExposureTime)}s</p>
+                <p>Aperture: f/{photoData?.exif_data?.exif?.FNumber}</p>
+                <p>ISO: {photoData?.exif_data?.exif?.ISO}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
+        ) : (
+          <div>Loading...</div>
+        )}
 
       <div className="delete-function">
-        {/* {photoData && (
+        {photoData && (
           <div className='delete-function__btn btn' onClick={handleDelete}>Delete</div>
-        )} */}
+        )}
       </div>
     </>
   );
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -21,7 +21,6 @@ export const formatGpsData = (gpsData, gpsRef) => {
   }
 };
 
-
 export const useDocumentTitle = (title) => {
   useEffect(() => {
     document.title = title;
